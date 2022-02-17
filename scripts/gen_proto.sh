318d13589bbe332cca575725c34d1050c6d8d442
James Hunsaker <hunsaker@spacetime.org>
2022-02-17 14:12:43 -0500

    add simple script for generating python protobuf

---
+++ scripts/gen_proto.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+GOGOPROTOBUF_REPO="${GIT_HOME}/gogoprotobuf"
+TENDERMINT_REPO="${GIT_HOME}/tendermint"
+
+protoc \
+  --proto_path="${GOGOPROTOBUF_REPO}" \
+  --proto_path="${TENDERMINT_REPO}/proto" \
+  --python_out=. \
+  "${TENDERMINT_REPO}/proto/tendermint/types/types.proto" \
+  "${TENDERMINT_REPO}/proto/tendermint/crypto/proof.proto" \
+  "${TENDERMINT_REPO}/proto/tendermint/version/types.proto" \
+  "${TENDERMINT_REPO}/proto/tendermint/types/validator.proto" \
+  "${TENDERMINT_REPO}/proto/tendermint/crypto/keys.proto" \
+  "${TENDERMINT_REPO}/proto/tendermint/types/block.proto" \
+  "${TENDERMINT_REPO}/proto/tendermint/types/evidence.proto" \
+  "${TENDERMINT_REPO}/proto/tendermint/store/types.proto" \
+
+touch tendermint/__init__.py
+touch tendermint/types/__init__.py
+touch tendermint/crypto/__init__.py
+touch tendermint/version/__init__.py
+touch tendermint/store/__init__.py
+
+protoc \
+  --proto_path="${GIT_HOME}/gogoprotobuf" \
+  --python_out=.. \
+  "${GIT_HOME}/gogoprotobuf/gogoproto/gogo.proto" \
